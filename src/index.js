0252e058f74eedffd2ccac9ca98c17ef63847677
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-19 19:14:07 +0100

    mofe global style and theme provider to app

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './core/App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from './themes/GlobalStyle';
-import { darkTheme } from './themes/theme';
+import store from './core/store';
+
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <GlobalStyle />
+    <Provider store={store}>
       <App />
-    </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
