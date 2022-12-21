d56a4d0d67b3869e1dd96e14613c21c6becf2f40
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-21 21:30:12 +0100

    change import

--- src/core/store.js
+++ src/core/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import themeReducer from "../reducers/themeSlice";
+import themeReducer from "../components/ThemeSwitch/themeSlice";
 
 const store = configureStore({
   reducer: {
