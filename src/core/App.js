d09a21f99be420eae2278c23f285fcc42d71fab3
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-21 21:31:43 +0100

    add Main and Known component to App

--- src/core/App.js
+++ src/core/App.js
@@ -1,21 +1,24 @@
-import { useSelector } from "react-redux";
-import { ThemeProvider } from "styled-components";
-import { selectIsDarkModeOn } from "../reducers/themeSlice";
-import { GlobalStyle } from "../themes/GlobalStyle";
-import { darkTheme, lightTheme } from "../themes/theme";
+import {useSelector} from "react-redux";
+import {ThemeProvider} from "styled-components";
+import {selectIsDarkModeOn} from "../components/ThemeSwitch/themeSlice";
+import {GlobalStyle} from "../themes/GlobalStyle";
+import {darkTheme, lightTheme} from "../themes/theme";
 import ThemeSwitch from "../components/ThemeSwitch";
+import Main from "../components/Main";
+import Known from "../features/skills/known";
 
 function App() {
 
   const isDarkModeOn = useSelector(selectIsDarkModeOn);
 
   return (
-    <>
-      <ThemeProvider theme={isDarkModeOn ? darkTheme : lightTheme}>
-        <GlobalStyle />
-        <ThemeSwitch />
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={isDarkModeOn ? darkTheme : lightTheme}>
+      <GlobalStyle/>
+      <Main>
+        <ThemeSwitch/>
+        <Known />
+      </Main>
+    </ThemeProvider>
   );
 }
 
