459e639a818d41847d11814d2063836c526bc894
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-22 17:20:30 +0100

    style section

---
+++ src/components/Section/index.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  width: 100%;
+  display: grid;
+  grid-template-rows: auto 1fr;
+  background: ${({theme}) => theme.colors.sectionBackground};
+  padding: 32px;
+  border-radius: 4px;
+  box-shadow: 0px -2px 50px ${({theme}) => theme.colors.shadow},
+              0px 16px 58px ${({theme}) => theme.colors.shadow};
+`;
