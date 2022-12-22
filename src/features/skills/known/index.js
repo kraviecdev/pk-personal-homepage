8a892e7b16e656a15509a3e7ae1726638cf2a506
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-22 17:21:28 +0100

    create known section

---
+++ src/features/skills/known/index.js
@@ -0,0 +1,18 @@
+import {Section} from "../../../components/Section";
+import {SectionTitle} from "../../../components/Title";
+import List from "../../../components/List";
+import jsonData from "../skills.json";
+const Known = () => {
+  const knownSkills = jsonData.known;
+
+  return (
+    <Section>
+      <SectionTitle>My skillset includes &#128640;</SectionTitle>
+      <List
+        data={knownSkills}
+      />
+    </Section>
+  )
+};
+
+export default Known;
