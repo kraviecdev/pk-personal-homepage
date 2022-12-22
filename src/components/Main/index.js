4c449a89cbb290e0e8eec1fdff2d14611ab9dc6b
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-22 17:20:15 +0100

    change max width, add gap and margin

---
+++ src/components/Main/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Main = styled.main`
+  display: grid;
+  max-width: 1200px;
+  position: relative;
+  gap: 72px;
+  margin: 115px auto;
+`;
+
+export default Main;
