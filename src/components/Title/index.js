8fb45a08878b129b1b513b1873c18526ca470049
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-23 06:06:04 +0100

    add media for smallest device (320px) change margin delete letter spacing

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -4,18 +4,18 @@ export const MainTitle = styled.h1`
   font-size: 38px;
   font-weight: 900;
   line-height: 46px;
-  letter-spacing: 0.05em;
+  margin: 12px 0;
   
   @media(max-width: ${({theme}) => theme.breakpoints.mobileS}px) {
     font-size: 22px;
     line-height: 26px;
+    margin: 8px 0;
   }
 `;
 export const SectionTitle = styled.h2`
   font-size: 30px;
   font-weight: 900;
   line-height: 36px;
-  letter-spacing: 0.05em;
   width: 100%;
   margin: 0;
   border-bottom: 1px solid ${({theme}) => theme.colors.border};
@@ -24,5 +24,6 @@ export const SectionTitle = styled.h2`
   @media (max-width: ${({theme}) => theme.breakpoints.mobileS}px) {
     font-size: 18px;
     line-height: 22px;
+    padding: 12px 0;
   }
 `;
