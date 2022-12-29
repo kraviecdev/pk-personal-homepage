44959f938dfddb147329a99194e0e9ba9bfadb02
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-29 19:44:01 +0100

    add project reducer & mount middleware

--- src/core/store.js
+++ src/core/store.js
@@ -1,10 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import themeReducer from "../components/ThemeSwitch/themeSlice";
+import projectReducer from "../features/projects/projectsSlice";
+import createSagaMiddleware from "@redux-saga/core";
+import rootSaga from "./rootSaga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     theme: themeReducer,
-  }
+    projects: projectReducer,
+  },
+  middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
