8ddd761341b29fed3aac033e1eb8d9dcdb98437d
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-29 19:50:32 +0100

    change placeholders for api endpoint

--- src/components/ProjectTile/index.js
+++ src/components/ProjectTile/index.js
@@ -1,30 +1,34 @@
-import {Tile, TileLink, TileLinkContainer, TileTitle} from "./styled";
+import {Tile, TileLink, TileLinkContainer, TileTitle, TileWrapper} from "./styled";
 import {DescriptionParagraph} from "../Description";
 
-const ProjectTile = () => (
-  <Tile>
-    <TileTitle>Example Title</TileTitle>
-    <DescriptionParagraph projectDescription>
-      Project description, e.g. website where you can search for favourite movies and people.
-      Project description, e.g. website where you can search.
-    </DescriptionParagraph>
-    <TileLinkContainer>
-      <DescriptionParagraph projectLink>
-        Demo:
-      </DescriptionParagraph>
-      <TileLink href="https://example.com/">
-        https://example.com/
-      </TileLink>
-    </TileLinkContainer>
-    <TileLinkContainer>
-      <DescriptionParagraph projectLink>
-        Code:
-      </DescriptionParagraph>
-      <TileLink href="https://example.com/">
-        https://example.com/
-      </TileLink>
-    </TileLinkContainer>
-  </Tile>
-);
+const ProjectTile = ({projects}) =>
+  (
+    <TileWrapper>
+      {projects.map(project => (
+        <Tile key={project.id}>
+          <TileTitle>{project.name}</TileTitle>
+          <DescriptionParagraph projectDescription>
+            {project.description}
+          </DescriptionParagraph>
+          <TileLinkContainer>
+            <DescriptionParagraph projectLink>
+              Demo:
+            </DescriptionParagraph>
+            <TileLink target="_blank" href={project.homepage}>
+              {project.homepage}
+            </TileLink>
+          </TileLinkContainer>
+          <TileLinkContainer>
+            <DescriptionParagraph projectLink>
+              Code:
+            </DescriptionParagraph>
+            <TileLink target="_blank" href={project.svn_url}>
+              {project.svn_url}
+            </TileLink>
+          </TileLinkContainer>
+        </Tile>
+      ))}
+    </TileWrapper>
+  );
 
 export default ProjectTile;
