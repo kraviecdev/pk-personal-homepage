cd3ac8604bf9bed98dffe06ef483cc66c1bec1e0
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-02 17:04:49 +0100

    change name & filter repositories

--- src/components/ProjectTile/index.js
+++ src/components/ProjectTile/index.js
@@ -5,8 +5,9 @@ const ProjectTile = ({projects}) =>
   (
     <TileWrapper>
       {projects.map(project => (
+        project.homepage &&
         <Tile key={project.id}>
-          <TileTitle>{project.name}</TileTitle>
+          <TileTitle>{project.name.replaceAll("-", " ")}</TileTitle>
           <DescriptionParagraph projectDescription>
             {project.description}
           </DescriptionParagraph>
@@ -17,13 +18,11 @@ const ProjectTile = ({projects}) =>
             <TileLink target="_blank" href={project.homepage}>
               {project.homepage}
             </TileLink>
-          </TileLinkContainer>
-          <TileLinkContainer>
             <DescriptionParagraph projectLink>
               Code:
             </DescriptionParagraph>
-            <TileLink target="_blank" href={project.svn_url}>
-              {project.svn_url}
+            <TileLink target="_blank" href={project.html_url}>
+              {project.html_url}
             </TileLink>
           </TileLinkContainer>
         </Tile>
