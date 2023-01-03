62aa79159d01c105764d8717b3ffa3e225816e79
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-03 18:17:06 +0100

    add color and code format

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,37 +1,39 @@
 const breakpoints = {
-    mobileS: "320",
-    mobileL: "475",
-    tablet: "768",
-    laptop: "1024",
-    laptopL: "1440"
+  mobileS: "320",
+  mobileL: "475",
+  tablet: "768",
+  laptop: "1024",
+  laptopL: "1440"
 };
 
 export const lightTheme = {
-    breakpoints,
-    colors: {
-        mainBackground: "#FBFBFE",
-        sectionBackground: "#FFFFFF",
-        switchBackground: "#E5E5E5",
-        border: "#D1D5DA4D",
-        mainFont: "#252525",
-        secondaryFont: "#6E7E91",
-        buttonFont: "#FFFFFF",
-        button: "#0366D6",
-        shadow: "#090A3308",
-    }
+  breakpoints,
+  colors: {
+    mainBackground: "#FBFBFE",
+    sectionBackground: "#FFFFFF",
+    switchBackground: "#E5E5E5",
+    border: "#D1D5DA4D",
+    mainFont: "#252525",
+    secondaryFont: "#6E7E91",
+    buttonFont: "#FFFFFF",
+    button: "#0366D6",
+    borderOnHover: "#0366D633",
+    shadow: "#090A3308",
+  }
 };
 
 export const darkTheme = {
-    breakpoints,
-    colors: {
-        mainBackground: "#252525",
-        sectionBackground: "#363636B8",
-        switchBackground: "#686868",
-        border: "#D1D5DA1A",
-        mainFont: "#FFFFFF",
-        secondaryFont: "#FFFFFF",
-        buttonFont: "#FFFFFF",
-        button: "#2188FF",
-        shadow: "#090A3305",
-    }
+  breakpoints,
+  colors: {
+    mainBackground: "#252525",
+    sectionBackground: "#363636B8",
+    switchBackground: "#686868",
+    border: "#D1D5DA1A",
+    mainFont: "#FFFFFF",
+    secondaryFont: "#FFFFFF",
+    buttonFont: "#FFFFFF",
+    button: "#2188FF",
+    borderOnHover: "#0366D680",
+    shadow: "#090A3305",
+  }
 };
