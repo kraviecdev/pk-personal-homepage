5ce6792c522128a6930327a6a3434ea188734014
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-04 19:35:03 +0100

    component name change

---
+++ src/components/Intro/index.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Intro = styled.span`
+  text-transform: uppercase;
+  color: ${({theme}) => theme.colors.secondaryFont};
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 16px;
+`;
