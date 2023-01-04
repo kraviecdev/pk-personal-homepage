6182a4cddb8c374ffe5270af073bbab9e8c0cb98
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-04 19:41:56 +0100

    add footer paragraph to component

--- src/features/footer/index.js
+++ src/features/footer/index.js
@@ -1,14 +1,14 @@
-import {FooterIconsWrapper, FooterMail, FooterWrapper} from "./styled";
-import {AdditionalInfo, DescriptionParagraph} from "../../components/Description";
+import {FooterIconsWrapper, FooterMail, FooterParagraph, FooterWrapper} from "./styled";
 import {GitHubIcon, LinkedInIcon} from "../../components/Icons";
+import {Intro} from "../../components/Intro";
 
 const Footer = () => (
   <FooterWrapper>
-    <AdditionalInfo>Let's talk</AdditionalInfo>
+    <Intro>Let's talk</Intro>
     <FooterMail href="mailto:patrick36212@gmail.com">patrick36212@gmail.com</FooterMail>
-    <DescriptionParagraph footerDescription>I’m always open to any suggestions and ideas.
+    <FooterParagraph>I’m always open to any suggestions and ideas.
       If you hava any questions or want to make a website or app, feel free to contact me through one of the following
-      way:</DescriptionParagraph>
+      way:</FooterParagraph>
     <FooterIconsWrapper>
       <a target="_blank" rel="noreferrer" href="https://github.com/patrick36212">
         <GitHubIcon/>
--- src/features/footer/styled.js
+++ src/features/footer/styled.js
@@ -5,7 +5,7 @@ export const FooterWrapper = styled.footer`
   grid-auto-rows: auto;
   max-width: 60%;
 
-  @media (max-width: ${({theme}) => theme.breakpoints.mobileS}px) {
+  @media (max-width: ${({theme}) => theme.breakpoints.mobileL}px) {
     max-width: 100%;
   }
 `;
@@ -23,13 +23,24 @@ export const FooterMail = styled.a`
     color: ${({theme}) => theme.colors.button};
   }
 
-  @media (max-width: ${({theme}) => theme.breakpoints.mobileS}px) {
+  @media (max-width: ${({theme}) => theme.breakpoints.mobileL}px) {
     font-size: 18px;
     line-height: 22px;
     margin: 12px 0;
   }
 `;
 
+export const FooterParagraph = styled.p`
+  font-size: 18px;
+  line-height: 26px;
+  margin: 24px 0;
+
+  @media (max-width: ${({theme}) => theme.breakpoints.mobileL}px) {
+    font-size: 14px;
+    line-height: 16px;
+    margin: 0 0 38px;
+  }
+`;
 export const FooterIconsWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(2, max-content);
