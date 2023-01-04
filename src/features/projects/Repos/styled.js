c131e5342a78e66b6caf01c96ffacbb5046c6ea4
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-04 19:40:54 +0100

    change component name and move to projects dir

--- src/features/projects/Repos/styled.js
+++ src/features/projects/Repos/styled.js
@@ -1,13 +1,17 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 export const TileWrapper = styled.article`
   display: grid;
-  grid-template-columns: repeat(2, minmax(288px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(388px, 1fr));
   gap: 32px;
 
-  @media (max-width: ${({theme}) => theme.breakpoints.mobileS}px) {
-    grid-template-columns: auto;
+  @media (max-width: ${({theme}) => theme.breakpoints.laptop}px) {
     gap: 16px;
+  }
+
+  @media (max-width: ${({theme}) => theme.breakpoints.tablet}px) {
+    grid-template-columns: 1fr;
+    gap: 8px;
   }
 `;
 
@@ -42,6 +46,40 @@ export const TileTitle = styled.h3`
     font-size: 16px;
     line-height: 20px;
   }
+`;
+
+export const TileDescription = styled.p`
+  color: ${({theme}) => theme.colors.secondaryFont};
+  font-size: 18px;
+  line-height: 26px;
+  text-align: justify;
+  overflow: auto;
+  margin: 24px 0;
+  max-height: 130px;
+
+  @media(max-width: ${({theme}) => theme.breakpoints.tablet}px) {
+    font-size: 16px;
+    line-height: 20px;
+    margin: 20px 0;
+  }
+
+  @media (max-width: ${({theme}) => theme.breakpoints.mobileL}px) {
+    font-size: 14px;
+    line-height: 16px;
+    margin: 16px 0;
+  }
+  
+  ${({projectLink}) => projectLink && css`
+    font-size: 18px;
+    line-height: 26px;
+    margin: 0;
+
+    @media (max-width: ${({theme}) => theme.breakpoints.mobileL}px) {
+      font-size: 14px;
+      line-height: 16px;
+      margin: 0;
+    }
+  `}
 `;
 
 export const TileLinkContainer = styled.div`
