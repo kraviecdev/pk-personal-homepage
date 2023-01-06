bb5780aeb7ef8e7a49827ad4bbf99a678d30b676
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-06 15:21:30 +0100

    implement Loading and Error state to Projects

--- src/features/projects/index.js
+++ src/features/projects/index.js
@@ -1,14 +1,17 @@
 import {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {fetchProjects, selectProjects} from "./projectsSlice";
+import {fetchProjects, selectProjects, selectProjectsStatus} from "./projectsSlice";
 import {Section, SectionSubtitle, SectionTitle} from "../../components/Section";
 import {GitHubIcon} from "../../components/Icons";
+import Loading from "./Info/Loading";
+import Error from "./Info/Error";
 import Repos from "./Repos";
 
 const Projects = () => {
 
   const dispatch = useDispatch();
   const projects = useSelector(selectProjects);
+  const status = useSelector(selectProjectsStatus);
 
   useEffect(() => {
     dispatch(fetchProjects());
@@ -20,9 +23,11 @@ const Projects = () => {
       <GitHubIcon sectionIcon/>
       <SectionTitle portfolio>Portfolio</SectionTitle>
       <SectionSubtitle> My recent projects</SectionSubtitle>
-      <Repos
+      {status === "loading" && <Loading/>}
+      {status === "error" && <Error/>}
+      {status === "success" && <Repos
         projects={projects}
-      />
+      />}
     </Section>
   )
 }
