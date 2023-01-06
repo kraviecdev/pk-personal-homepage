2e732c57cc2b9754b5fad16122d536208c6d3c2a
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-06 15:14:51 +0100

    create styled.js with error icon

---
+++ src/features/projects/Info/Error/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import {ReactComponent as Error} from "./error.svg";
+export const ErrorIcon = styled(Error)`
+  fill: ${({theme}) => theme.colors.mainFont};
+  
+  @media (max-width: ${({theme}) => theme.breakpoints.tablet}px) {
+    width: 38px;
+  };
+
+  @media (max-width: ${({theme}) => theme.breakpoints.mobileL}px) {
+    width: 32px;
+  };
+`;
