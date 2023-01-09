c70785a3c016ae6a4bc509ecff4888886e799bd9
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-09 19:06:49 +0100

    make code more readable

--- src/features/footer/index.js
+++ src/features/footer/index.js
@@ -1,14 +1,16 @@
 import {FooterIconsWrapper, FooterMail, FooterParagraph, FooterWrapper} from "./styled";
 import {GitHubIcon, LinkedInIcon} from "../../components/Icons";
-import {Intro} from "../../components/Intro";
+import Intro from "../../components/Intro";
 
 const Footer = () => (
   <FooterWrapper>
     <Intro>Let's talk</Intro>
     <FooterMail href="mailto:patrick36212@gmail.com">patrick36212@gmail.com</FooterMail>
-    <FooterParagraph>I’m always open to any suggestions and ideas.
+    <FooterParagraph>
+      I’m always open to any suggestions and ideas.
       If you hava any questions or want to make a website or app, feel free to contact me through one of the following
-      way:</FooterParagraph>
+      way:
+    </FooterParagraph>
     <FooterIconsWrapper>
       <a target="_blank" rel="noreferrer" href="https://github.com/patrick36212">
         <GitHubIcon/>
--- src/features/header/index.js
+++ src/features/header/index.js
@@ -1,31 +1,31 @@
-import {Intro} from "../../components/Intro";
-import {MainButton} from "../../components/Button";
 import {ContentWrapper, HeaderImg, HeaderParagraph, HeaderTitle, HeaderWrapper} from "./styled";
+import Intro from "../../components/Intro";
+import Button from "../../components/Button";
 import ThemeSwitch from "./ThemeSwitch";
 
-const Header = () => {
-  return (
+const Header = () => (
     <HeaderWrapper>
       <ThemeSwitch/>
-      <HeaderImg></HeaderImg>
+      <HeaderImg/>
       <ContentWrapper>
         <Intro>This is</Intro>
         <HeaderTitle>Patryk Krawczyk</HeaderTitle>
-        <HeaderParagraph>&#129333;&#128187; a passionate Front-End Developer.
+        <HeaderParagraph>
+          &#129333;&#128187; a passionate Front-End Developer.
           JavaScript is my Language, React is how i speak.
           I am experienced with responsive frontend design.
-          Fast learner, open to new opportunities.</HeaderParagraph>
-        <MainButton
+          Fast learner, open to new opportunities.
+        </HeaderParagraph>
+        <Button
           onClick={() => window.scrollTo({
             top: document.body.scrollHeight,
             behavior: "smooth"
           })}
         >
           &#9993; Contact Me
-        </MainButton>
+        </Button>
       </ContentWrapper>
     </HeaderWrapper>
   );
-};
 
 export default Header;
