061f12f0c84af143356be14c27046d2b9ff4b153
patrick36212 <patrick36212@gmail.com>
2024-02-16 09:45:59 +0100

    add sorting fn

--- src/features/skills/SkillsList/index.js
+++ src/features/skills/SkillsList/index.js
@@ -2,7 +2,7 @@ import {ListItem, UnorderedList} from "./styled";
 
 const SkillsList = ({data}) => (
   <UnorderedList>
-    {data.map(item => (
+    {data.sort((a, b) => a.name.localeCompare(b.name)).map(item => (
       <ListItem key={item.id}>
         {item.name}
       </ListItem>
