a87daa4c63ee6b502668ee343e24e0405bc55d73
patrick36212 <patrick36212@gmail.com>
2024-03-10 11:38:40 +0100

    delete tests

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './core/App';
-import reportWebVitals from './reportWebVitals';
 import store from './core/store';
 
 import {Provider} from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
       <App/>
     </Provider>
-  </React.StrictMode>
 );
-
-reportWebVitals();
